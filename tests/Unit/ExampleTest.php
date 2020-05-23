06e90083062b665256d3aa3073a4eef175aefa95
Cassio Almeida <cassio@inat.com.br>
2020-05-23 15:33:50 -0300

    Checking if a product has stock ar retailer

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,32 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Product;
+use App\Retailer;
+use App\Stock;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_checks_for_stock_ar_retailers()
     {
-        $this->assertTrue(true);
+        $switch = Product::create(['name' => 'Nintendo Stwich']);
+        $bestBy = Retailer::create(['name' => 'Best Buy']);
+        $this->assertFalse($switch->inStock());
+
+        $stock = new Stock([
+            'price' => 1000,
+            'url' => 'https://foo.com',
+            'sku' => '12345',
+            'in_stock' => true
+        ]);
+
+        $bestBy->addStock($switch, $stock);
+
+        $this->assertTrue($switch->inStock());
     }
 }
